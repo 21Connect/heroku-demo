c36d0e55ba901657a3bfd25cc8dc04ee2df23583
Tom Trahan <tom@shippable.com>
2017-01-16 13:59:32 -0800

    fixes path to write code coverage results

--- test/test_4.sh
+++ test/test_4.sh
@@ -8,6 +8,6 @@ mocha test/mocha/$baseFile.js
 mocha test/mocha/$baseFile.js -R mocha-junit-reporter --reporter-options mochaFile=$TEST_RESULTS_DIR/testresults.xml
 cd test/mocha
 istanbul cover _mocha $baseFile.js
-istanbul report cobertura --dir $CODE_COVERAGE_DIR
+istanbul report cobertura --dir ../../$CODE_COVERAGE_DIR
 
 echo "test 4 group completed"
