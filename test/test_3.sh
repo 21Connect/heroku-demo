d9c0dc0b6cd71dc9ee10081612bc7f4a684f0c65
Tom Trahan <tom@shippable.com>
2017-01-16 14:54:13 -0800

    update tests to run sequentially

--- test/test_3.sh
+++ test/test_3.sh
@@ -3,8 +3,13 @@
 echo "running test group 3"
 sleep 15s
 
-baseFile=${TEST_TO_RUN%.sh}
-mocha test/mocha/$baseFile.js
+if [ -z $TEST_TO_RUN ]; then
+  baseFile=test_3
+else
+  baseFile=${TEST_TO_RUN%.sh}
+fi
+
+# mocha test/mocha/$baseFile.js
 mocha test/mocha/$baseFile.js -R mocha-junit-reporter --reporter-options mochaFile=$TEST_RESULTS_DIR/testresults.xml
 cd test/mocha
 istanbul cover _mocha $baseFile.js
