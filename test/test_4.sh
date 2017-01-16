2d4c3c4a1339d03e7d118dd95f0f640061e68ac4
Tom Trahan <tom@shippable.com>
2017-01-16 13:57:21 -0800

    update code coverage tests with correct path

--- test/test_4.sh
+++ test/test_4.sh
@@ -7,7 +7,7 @@ baseFile=${TEST_TO_RUN%.sh}
 mocha test/mocha/$baseFile.js
 mocha test/mocha/$baseFile.js -R mocha-junit-reporter --reporter-options mochaFile=$TEST_RESULTS_DIR/testresults.xml
 cd test/mocha
-istanbul cover _mocha *.js
+istanbul cover _mocha $baseFile.js
 istanbul report cobertura --dir $CODE_COVERAGE_DIR
 
 echo "test 4 group completed"
